f5d559edbc51690f2203b203ee8ea9af4d1b2133
pixelberry-jin <viva-jinyi@kakao.com>
2024-12-01 12:42:36 +0900

    fix: page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import ThemeToggle from "@/components/ThemeToggle";
-
 export default function Home() {
   return (
     <div>
-      <div className="float-right mx-2 my-2">
-        <ThemeToggle />
-      </div>
+      Home
     </div>
   );
 }
